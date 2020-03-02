7dcf3fe5b7aaec1b984c2e12745dc43dea756346
= <=>
2020-03-02 16:28:02 -0300

    Alterei a frase de acordo com o exercício novamente

--- Program.cs
+++ Program.cs
@@ -6,7 +6,24 @@ namespace musica
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Música PSYCHO!");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("I might just go psycho");
+            Console.WriteLine("Too many drunk white hoes");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("I might just go psycho");
+            Console.WriteLine("Too many drunk white hoes");
+            Console.WriteLine("Said I won't, but I might though");
+            Console.WriteLine("Walking on the tight rope");
+            Console.WriteLine("Might do it just in spite though");
+            Console.WriteLine("Backwoods got my eyes low");
+            Console.WriteLine("I might just go psycho");
+            Console.WriteLine("Backwoods had my eyes low");
+            Console.WriteLine("I might just go psycho Too many drunk white hoes");
+
+
         }
     }
 }
